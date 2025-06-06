ec5454529dfb34710be28212bccc4df2fd287a9b
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-06 16:20:11 +0200

    add input kms and imput age to html form; add placeholder text for inputs; add DOM nodes for html inputs and addEventListener to button to print user data to console

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/* 
+Scrivere un programma che chieda all’utente:
+ Il numero di chilometri da percorrere
+- Età del passeggero
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+*/
+
+const kmFieldEl = document.getElementById("kms");
+const ageFieldEl = document.getElementById("age");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  const travelDistance = Number(kmFieldEl.value);
+  const userAge = Number(ageFieldEl.value);
+  console.log(travelDistance)
+  console.log(userAge);
+  
+});
