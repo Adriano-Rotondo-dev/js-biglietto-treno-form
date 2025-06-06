de020f5c8e7afcec946e82e860fc60484f99caa7
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-06 17:05:14 +0200

    add ticketPrice calculator based on pricePerKm variable, add conditions to change price based on userAge inserted value, refactor main.js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 - va applicato uno sconto del 40% per gli over 65.
 */
 //* salvo in una costante il DOM node dell'input name
-const nameFieldEl = document.getElementById("name")
+const nameFieldEl = document.getElementById("name");
 //* salvo in una costante il DOM node dell'input kms
 const kmFieldEl = document.getElementById("kms");
 //* salvo in una costante il DOM node dell'input age
@@ -24,23 +24,23 @@ submitButtonEl.addEventListener("click", () => {
   const userName = nameFieldEl.value;
   const travelDistance = Number(kmFieldEl.value);
   const userAge = Number(ageFieldEl.value);
-  console.log(userName)
+  const pricePerKm = 0.21;
+  console.log(userName);
   console.log(travelDistance);
   console.log(userAge);
+  //*salvo in una variabile il prezzo del biglietto
+  let ticketPrice = travelDistance * pricePerKm;
+  //*stabilisco le condizioni di scontistica del biglietto in base ad ageFieldEl.value
+  if (userAge < 18) {
+    finalPrice = ticketPrice * 0.8;
+  } else if (userAge > 65) {
+    finalPrice = ticketPrice * 0.6;
+  } else if (userAge > 18 && userAge < 65) {
+    finalPrice = ticketPrice;
+  }
+  //*stampo in console il prezzo finale, mostrando fino a 2 decimali
+  console.log(finalPrice.toFixed(2));
 });
+
 //* inizializzo l'eventListener del bottone cancel per ricaricare la pagina
 cancelButtonEl.addEventListener("click", () => location.reload());
-
-
-/*
-const pricePerKm = (0.21); 
-const ticketPrice = Number(userKms * pricePerKm );
-
-if (userAge <= 18 ) {
-    console.log( "Il tuo biglietto scontato costa €" + (ticketPrice * 0.8).toFixed(2) ) 
-} else if (userAge >= 65) {
-    console.log( "Il tuo biglietto scontato costa €" + (ticketPrice * 0.6).toFixed(2) ) 
-} else {
-    console.log("il tuo biglietto costa €" + (ticketPrice).toFixed(2) ) //inserita un'alternativa priva di scontistica per non ripetere il messaggio precedente
-};
-*/
